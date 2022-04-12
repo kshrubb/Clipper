0f414bf76a486aa6a47653f7b406aa62433f752a
kshrubb <hamwill@live.com>
2022-04-11 21:17:11 -0400

    Revert "Finalized features for SSBM Falcord"
    This reverts commit bb7af7e5da0bcdecd58bf35a1941c19a63ca76f7.

--- main.py
+++ main.py
@@ -16,29 +16,26 @@ if len(server_settings) == 0:
     quit()
 
 load_dotenv()
-if os.getenv('BOT_TOKEN') is None:
+if os.getenv('BOT_TOKEN') == None:
     print("You need to add a BOT_TOKEN to .env! .env may be hidden in the directory since it's a system file. ")
     quit()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 IDS = [i for i in server_settings]
+print(server_settings)
 
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 intents.message_content = True
 intents.guilds = True
-intents.guild_messages = True
 
 bot = discord.Bot(intents=intents)
 
-realLinks = ["https://gfycat.com/", "https://youtube.com/", "https://twitch.tv/",
+realLinks = ["https://gfycat.com/", "https://www.youtube.com/", "https://www.twitch.tv/",
              "https://imgur.com/", "https://streamable.com/", "https://youtu.be/",
              "https://clips.twitch.tv/", "https://twitter.com/", "https://fxtwitter.com/",
-             "https://giant.gfycat.com/", "https://www.gfycat.com/", "https://www.youtube.com/", "https://www.twitch.tv/",
-             "https://www.imgur.com/", "https://www.streamable.com/", "https://www.youtu.be/",
-             "https://www.clips.twitch.tv/", "https://www.twitter.com/", "https://www.fxtwitter.com/",
-             "https://www.giant.gfycat.com/"]
+             "https://giant.gfycat.com/"]
 
 
 @bot.event
@@ -58,7 +55,6 @@ async def on_guild_join(guild):
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
-    print(f"Connected servers: {str([i.name for i in bot.guilds])}")
 
 
 @bot.event
@@ -71,7 +67,7 @@ async def on_message(message):
 
 @bot.slash_command(guild_ids=IDS, description="Set the clips channel with the channel ID.")
 async def set_clips(ctx, channel_id):
-    if (ctx.author.guild_permissions.administrator) or (ctx.author.id == 142116202808999936):
+    if ctx.author.guild_permissions.administrator:
         server_settings[str(ctx.guild.id)]["CLIPS_CHANNEL_ID"] = channel_id
         with open("servers.json", 'w') as outfile:
             json.dump(server_settings, outfile, indent=4)
@@ -81,7 +77,7 @@ async def set_clips(ctx, channel_id):
 
 @bot.slash_command(guild_ids=IDS, description="Set the clipper clip ratelimit. ")
 async def set_ratelimit(ctx, ratelimit):
-    if (ctx.author.guild_permissions.administrator) or (ctx.author.id == 142116202808999936):
+    if ctx.author.guild_permissions.administrator:
         server_settings[str(ctx.guild.id)]["RATE_LIMITER"] = ratelimit
         with open("servers.json", 'w') as outfile:
             json.dump(server_settings, outfile, indent=4)
@@ -92,49 +88,39 @@ async def set_ratelimit(ctx, ratelimit):
 @bot.slash_command(guild_ids=IDS, description="/clip <link> <optional thread_name>")
 async def clip(ctx, link: str, thread_name: str = None):
     CLIPS_CHANNEL_ID = server_settings[str(ctx.guild.id)]["CLIPS_CHANNEL_ID"]
-    if CLIPS_CHANNEL_ID != "null":
-        rl = int(server_settings[str(ctx.guild.id)]["RATE_LIMITER"])
-        hour_ago = datetime.datetime.now() - datetime.timedelta(hours=1)
-        counter = 0
-        async for msg in ctx.channel.history(limit=100, after=hour_ago):
-            counter += 1
-        if (rl > 0) and (counter >= rl):
-            await ctx.respond(f"<@{ctx.author.id}> You are posting too many clips. Try again in about an hour. ", delete_after=8)
-        else:
-            submitter = ctx.author
-            if thread_name is None:
-                thread_name = f"Combo by {str(submitter.name).split('#')[0]}!"
-            if len(thread_name) < 50:
-                if str(ctx.channel.id) == CLIPS_CHANNEL_ID:
-                    if any(site in link for site in realLinks):
-                        await ctx.respond("Posting! ", delete_after=0)
-                        pfp = ctx.author.avatar.url
-                        playercard = discord.Embed(
-                            title=f"{thread_name}",
-                            color=discord.Color.blue()
-                        )
-                        playercard.set_author(name=ctx.author.name, icon_url=pfp)
-                        playercard.set_thumbnail(url=pfp)
-                        await ctx.send(embed=playercard)
-                        pre_thread = await ctx.send(f"{link}")
-                        await pre_thread.add_reaction("🔥")
-                        new_thread = await pre_thread.create_thread(name=thread_name)
-                        await new_thread.add_user(submitter)
-                        print("A new clip was submitted successfully.")
-                    else:
-                        await ctx.respond(f"<@{ctx.author.id}> Submission failed: Try /clipperhelp to see available sites.", delete_after=8)
-                else:
-                    await ctx.respond(f"<@{ctx.author.id}> You are submitting in the incorrect channel. ", delete_after=6)
-            else:
-                await ctx.respond(f"<@{ctx.author.id}> Try a shorter thread name. ", delete_after=6)
+    rl = int(server_settings[str(ctx.guild.id)]["RATE_LIMITER"])
+    hour_ago = datetime.datetime.now() - datetime.timedelta(hours=1)
+    counter = 0
+    async for msg in ctx.channel.history(limit=None, after=hour_ago):
+        counter += 1
+    print(counter)
+    if (rl > 0) and (counter >= rl):
+        await ctx.respond(f"<@{ctx.author.id}> You are posting too many clips. Try again in about an hour. ", delete_after=8)
     else:
-        await ctx.respond(f"<@{ctx.author.id}>The #clips channel ID needs to be set. ", delete_after=6)
+        submitter = ctx.author
+        if thread_name is None:
+            thread_name = f"Combo by {str(submitter.name).split('#')[0]}!"
+        if len(thread_name) < 50:
+            if str(ctx.channel.id) == CLIPS_CHANNEL_ID:
+                if any(site in link for site in realLinks):
+                    await ctx.respond("Posting! ", delete_after=0)
+                    pre_thread = await ctx.send(f"{link}")
+                    new_thread = await pre_thread.create_thread(name=thread_name)
+                    await new_thread.add_user(submitter)
+                    print("A new clip was submitted successfully.")
+                else:
+                    await ctx.respond(f"<@{ctx.author.id}> Submission failed: Try /clipperhelp to see available sites.", delete_after=8)
+            else:
+                await ctx.respond(f"<@{ctx.author.id}>The #clips channel ID needs to be set. ", delete_after=4)
+        else:
+            await ctx.respond(f"<@{ctx.author.id}> Try a shorter thread name. ", delete_after=4)
+
 
 @bot.slash_command(guild_ids=IDS)
 async def clipperhelp(ctx):
     userEmbed = discord.Embed(title="Clipper User Help", color=0x03a5fc)
-    accepted_sites = [i for i in realLinks if "https://www." not in i]
-    userEmbed.add_field(name="Accepted Sites", value=f"{accepted_sites}")
+    userEmbed.add_field(name="Accepted Sites", value=f"{realLinks}")
+    userEmbed.set_image(url="https://i.imgur.com/Tlg0FiA.png")
     adminEmbed = discord.Embed(title="Clipper Admin Help", color=0x03a5fc)
     adminEmbed.add_field(name="/botmessage", value="Creates a bot message in #clips .")
     adminEmbed.add_field(name="/my_guild", value="Returns server json fields to only the invoker.")
@@ -142,21 +128,20 @@ async def clipperhelp(ctx):
     adminEmbed.add_field(name="/set_ratelimit <ratelimit>",
                         value="Sets the amount of clips that can be sent per user per hour. Set to 0 by default, meaning no limit to clips sent.")
 
-    await ctx.respond(embeds=(userEmbed, adminEmbed), delete_after=60)
-    await ctx.send("https://gfycat.com/webbedevilindusriverdolphin", delete_after=60)
+    await ctx.respond("boo", delete_after=0)
+    await ctx.send(embeds=(userEmbed, adminEmbed), delete_after=60)
 
 
 @bot.slash_command(guild_ids=IDS, description="Send a message from Clipper. ")
 async def botmessage(ctx, message):
-    if (ctx.author.guild_permissions.administrator) or (ctx.author.id == 142116202808999936):
+    if ctx.author.guild_permissions.administrator:
         await ctx.respond("boo", delete_after=0)
         await ctx.send(message)
 
 
 @bot.slash_command(guild_ids=IDS, description="Returns server information. ")
 async def my_guild(ctx):
-    if (ctx.author.guild_permissions.administrator) or (ctx.author.id == 142116202808999936):
+    if ctx.author.guild_permissions.administrator:
         await ctx.respond(f"{server_settings[str(ctx.guild.id)]}", delete_after=20)
-
 
 bot.run(BOT_TOKEN)
